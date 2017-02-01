1d15370c8e12f592f4cf7be1c1313260c5beca9e
Student <Student@VM-macOS-1012.local>
2017-02-01 09:13:52 +0100

    Initial Commit

---
+++ Assignment07/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment07
+//
+//  Created by Student on 01/02/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
